e7bf333a939c1a787c58c85b5a615e92dc44e9f8
Brijen <bshah91@my.centennialcollege.ca>
2023-02-05 20:11:37 -0500

    first commit

--- MAPD724_ICE3_Brijen/GameScene.swift
+++ MAPD724_ICE3_Brijen/GameScene.swift
@@ -1,76 +1,92 @@
-//
-//  GameScene.swift
-//  MAPD724_ICE3_Brijen
-//
-//  Created by Brijen Shah on 2023-02-05.
-//
-
+import UIKit
+import AVFoundation
 import SpriteKit
 import GameplayKit
 
+var screenSize = UIScreen.main.bounds
+var screenWidth: CGFloat?
+var screenHeight: CGFloat?
+
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    var ocean1: Ocean?
+    var ocean2: Ocean?
+    var plane: Plane?
+    var island: Island?
+    var clouds: [Cloud] = []
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+override func sceneDidLoad()
+    {
+        screenWidth = frame.width
+        screenHeight = frame.height
+        name = "FIRST GAME"
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        //add ocean1 to the game scene
+        ocean1 = Ocean()
+        ocean1?.Reset()
+        addChild(ocean1!)
+        
+        //add ocean2 to the game scene
+        ocean2 = Ocean()
+        ocean2?.position.y = -627
+        addChild(ocean2!)
+        
+        //add plane to the Scene
+        plane = Plane()
+        addChild(plane!)
+        
+        //add island to the scene
+        island = Island()
+        addChild(island!)
+        
+        //add 3 clouds to the scene
+        for _ in 0...2
+        {
+            let cloud: Cloud = Cloud()
+            clouds.append(cloud)
+            addChild(cloud)
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // Engine Sound
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        let engineSound = SKAudioNode(fileNamed: "engine.mp3")
+        addChild(engineSound)
+        engineSound.autoplayLooped = true
+        
+        //preload the sounds
+        do
+        {
+            let sounds: [String] = ["thunder", "yay"]
+            for sound in sounds
+            {
+                let path: String = Bundle.main.path(forResource: sound, ofType: "mp3") ?? ""
+                let url: URL = URL(fileURLWithPath: path)
+                let avPlane: AVAudioPlayer = try AVAudioPlayer(contentsOf: url)
+                avPlane.prepareToPlay()
+            }
+        }
+        catch
+        {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
         }
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func touchDown(atPoint pos : CGPoint)
+    {
+        plane?.TouchMove(newPos: CGPoint(x: pos.x, y: -640))
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        plane?.TouchMove(newPos: CGPoint(x: pos.x, y: -640))
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        plane?.TouchMove(newPos: CGPoint(x: pos.x, y: -640))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
@@ -88,22 +104,20 @@ class GameScene: SKScene {
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    override func update(_ currentTime: TimeInterval)
+    {
+        ocean1?.Update()
+        ocean2?.Update()
+        plane?.Update()
+        island?.Update()
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        CollisionManager.SquaredRadiusCheck(scene: self, object1: plane!, object2: island!)
+        
+        //update each cloud in clouds
+        for cloud in clouds
+        {
+            cloud.Update()
+            CollisionManager.SquaredRadiusCheck(scene: self, object1: plane!, object2: cloud)
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
